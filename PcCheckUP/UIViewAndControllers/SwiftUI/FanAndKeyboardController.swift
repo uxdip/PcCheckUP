1770cd3323dc41ae9d0cc0f2f47342f7831b21ed
sazid <mdip@uxd.co.jp>
2022-07-13 16:53:23 +0900

    commit for name change

---
+++ PcCheckUP/UIViewAndControllers/SwiftUI/FanAndKeyboardController.swift
@@ -0,0 +1,20 @@
+//
+//  FanAndKeyboardController.swift
+//  PcCheckUP
+//
+//  Created by Apple on 2022/07/12.
+//
+
+import SwiftUI
+
+struct FanAndKeyboardController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FanAndKeyboardController_Previews: PreviewProvider {
+    static var previews: some View {
+        FanAndKeyboardController()
+    }
+}
